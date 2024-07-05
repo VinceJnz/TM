c2ac4fd9f5f9a022b399662c41072ec865e26a1f
unknown <vince.jennings@gmail.com>
2024-07-05 18:55:59 +1200

    moved functions and html into wasm

--- client1/main.go
+++ client1/main.go
@@ -15,6 +15,9 @@ func main() {
 	js.Global().Set("toggleSideMenu", js.FuncOf(toggleSideMenu))
 	js.Global().Set("fetchUserData", js.FuncOf(fetchUserData))
 
+	// Set up the HTML structure
+	setupHTML()
+
 	// Add event listeners for the buttons
 	js.Global().Get("document").Call("getElementById", "fetchDataBtn").Call("addEventListener", "click", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		fetchUserData(js.Value{}, []js.Value{})
@@ -37,6 +40,59 @@ func main() {
 	}))
 
 	<-c
+}
+
+func setupHTML() {
+	document := js.Global().Get("document")
+
+	// Create new body element
+	newBody := document.Call("createElement", "body")
+
+	navbar := document.Call("createElement", "div")
+	navbar.Set("className", "navbar")
+	newBody.Call("appendChild", navbar)
+
+	menuIcon := document.Call("createElement", "div")
+	menuIcon.Set("id", "menuIcon")
+	menuIcon.Set("innerHTML", "&#9776;")
+	navbar.Call("appendChild", menuIcon)
+
+	fetchDataBtn := document.Call("createElement", "button")
+	fetchDataBtn.Set("id", "fetchDataBtn")
+	fetchDataBtn.Set("innerHTML", "Fetch User Data")
+	navbar.Call("appendChild", fetchDataBtn)
+
+	createDropdownBtn := document.Call("createElement", "button")
+	createDropdownBtn.Set("id", "createDropdownBtn")
+	createDropdownBtn.Set("innerHTML", "Create Dropdown")
+	navbar.Call("appendChild", createDropdownBtn)
+
+	createSideMenuBtn := document.Call("createElement", "button")
+	createSideMenuBtn.Set("id", "createSideMenuBtn")
+	createSideMenuBtn.Set("innerHTML", "Create Side Menu")
+	navbar.Call("appendChild", createSideMenuBtn)
+
+	sidemenu := document.Call("createElement", "div")
+	sidemenu.Set("id", "sideMenu")
+	sidemenu.Set("className", "sidemenu")
+	sidemenu.Set("innerHTML", `<a href="javascript:void(0)" class="closebtn" onclick="toggleSideMenu()">&times;</a>
+                               <a href="#">Home</a>
+                               <a href="#">About</a>
+                               <a href="#">Contact</a>`)
+	newBody.Call("appendChild", sidemenu)
+
+	mainContent := document.Call("createElement", "div")
+	mainContent.Set("id", "mainContent")
+	mainContent.Set("className", "main")
+	newBody.Call("appendChild", mainContent)
+
+	output := document.Call("createElement", "div")
+	output.Set("id", "output")
+	output.Set("className", "output")
+	mainContent.Call("appendChild", output)
+
+	// Replace the existing body with the new body
+	document.Get("documentElement").Call("replaceChild", newBody, document.Get("body"))
 }
 
 func createDropdown(this js.Value, p []js.Value) interface{} {
@@ -106,15 +162,14 @@ func showSideMenu(this js.Value, p []js.Value) interface{} {
 func toggleSideMenu(this js.Value, p []js.Value) interface{} {
 	document := js.Global().Get("document")
 	sideMenu := document.Call("getElementById", "sideMenu")
-	if sideMenu.Truthy() {
-		display := sideMenu.Get("style").Get("display").String()
-		if display == "none" {
-			sideMenu.Get("style").Set("display", "block")
-			document.Get("body").Get("classList").Call("add", "shifted") // Add the shifted class
-		} else {
-			sideMenu.Get("style").Set("display", "none")
-			document.Get("body").Get("classList").Call("remove", "shifted") // Remove the shifted class
-		}
+	mainContent := document.Call("getElementById", "mainContent")
+
+	if sideMenu.Get("style").Get("width").String() == "250px" {
+		sideMenu.Get("style").Set("width", "0")
+		mainContent.Get("style").Set("marginLeft", "0")
+	} else {
+		sideMenu.Get("style").Set("width", "250px")
+		mainContent.Get("style").Set("marginLeft", "250px")
 	}
 	return nil
 }
