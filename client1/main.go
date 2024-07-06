2bd79b29d81f632d221d2b0078fcda9532d4c4a9
unknown <vince.jennings@gmail.com>
2024-07-06 19:36:42 +1200

    separated user ui to separate package

--- client1/main.go
+++ client1/main.go
@@ -1,24 +1,134 @@
 package main
 
 import (
-	"encoding/json"
-	"net/http"
+	"client1/v2/views/user"
 	"syscall/js"
 )
 
 func main() {
 	c := make(chan struct{})
-	js.Global().Set("createDropdown", js.FuncOf(createDropdown))
-	js.Global().Set("createSideMenu", js.FuncOf(createSideMenu))
-	js.Global().Set("hideSideMenu", js.FuncOf(hideSideMenu))
-	js.Global().Set("showSideMenu", js.FuncOf(showSideMenu))
-	js.Global().Set("toggleSideMenu", js.FuncOf(toggleSideMenu))
-	js.Global().Set("fetchUserData", js.FuncOf(fetchUserData))
 
 	// Set up the HTML structure
 	setupHTML()
 
+	editor := user.NewUserEditor()
+
+	// Bind methods
+	js.Global().Set("toggleSideMenu", js.FuncOf(toggleSideMenu))
+
 	// Add event listeners for the buttons
+	js.Global().Get("document").Call("getElementById", "fetchDataBtn").Call("addEventListener", "click", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		editor.FetchUserData(js.Value{}, []js.Value{})
+		return nil
+	}))
+
+	js.Global().Get("document").Call("getElementById", "menuIcon").Call("addEventListener", "click", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		toggleSideMenu(js.Value{}, []js.Value{})
+		return nil
+	}))
+
+	js.Global().Get("document").Call("getElementById", "submitEditBtn").Call("addEventListener", "click", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		editor.SubmitUserEdit(js.Value{}, []js.Value{})
+		return nil
+	}))
+
+	<-c
+}
+
+func setupHTML() {
+	document := js.Global().Get("document")
+
+	// Create new body element
+	newBody := document.Call("createElement", "body")
+
+	navbar := document.Call("createElement", "div")
+	navbar.Set("className", "navbar")
+	newBody.Call("appendChild", navbar)
+
+	menuIcon := document.Call("createElement", "div")
+	menuIcon.Set("id", "menuIcon")
+	menuIcon.Set("innerHTML", "&#9776;")
+	navbar.Call("appendChild", menuIcon)
+
+	fetchDataBtn := document.Call("createElement", "button")
+	fetchDataBtn.Set("id", "fetchDataBtn")
+	fetchDataBtn.Set("innerHTML", "Fetch User Data")
+	navbar.Call("appendChild", fetchDataBtn)
+
+	createDropdownBtn := document.Call("createElement", "button")
+	createDropdownBtn.Set("id", "createDropdownBtn")
+	createDropdownBtn.Set("innerHTML", "Create Dropdown")
+	navbar.Call("appendChild", createDropdownBtn)
+
+	createSideMenuBtn := document.Call("createElement", "button")
+	createSideMenuBtn.Set("id", "createSideMenuBtn")
+	createSideMenuBtn.Set("innerHTML", "Create Side Menu")
+	navbar.Call("appendChild", createSideMenuBtn)
+
+	sidemenu := document.Call("createElement", "div")
+	sidemenu.Set("id", "sideMenu")
+	sidemenu.Set("className", "sidemenu")
+	sidemenu.Set("innerHTML", `<a href="javascript:void(0)" class="closebtn" onclick="toggleSideMenu()">&times;</a>
+							   <a href="#">Home</a>
+							   <a href="#">About</a>
+							   <a href="#">Contact</a>`)
+	newBody.Call("appendChild", sidemenu)
+
+	mainContent := document.Call("createElement", "div")
+	mainContent.Set("id", "mainContent")
+	mainContent.Set("className", "main")
+	newBody.Call("appendChild", mainContent)
+
+	output := document.Call("createElement", "div")
+	output.Set("id", "output")
+	output.Set("className", "output")
+	mainContent.Call("appendChild", output)
+
+	editForm := document.Call("createElement", "form")
+	editForm.Set("id", "editForm")
+	editForm.Get("style").Set("display", "none")
+	editForm.Set("innerHTML", `
+		<label for="userName">Name:</label>
+		<input type="text" id="userName" name="userName"><br>
+		<label for="userUsername">Username:</label>
+		<input type="text" id="userUsername" name="userUsername"><br>
+		<label for="userEmail">Email:</label>
+		<input type="email" id="userEmail" name="userEmail"><br>
+		<button type="button" id="submitEditBtn">Submit</button>
+	`)
+	mainContent.Call("appendChild", editForm)
+
+	// Replace the existing body with the new body
+	document.Get("documentElement").Call("replaceChild", newBody, document.Get("body"))
+}
+
+func toggleSideMenu(this js.Value, p []js.Value) interface{} {
+	document := js.Global().Get("document")
+	sideMenu := document.Call("getElementById", "sideMenu")
+	mainContent := document.Call("getElementById", "mainContent")
+
+	if sideMenu.Get("style").Get("width").String() == "250px" {
+		sideMenu.Get("style").Set("width", "0")
+		mainContent.Get("style").Set("marginLeft", "0")
+	} else {
+		sideMenu.Get("style").Set("width", "250px")
+		mainContent.Get("style").Set("marginLeft", "250px")
+	}
+	return nil
+}
+
+/*
+func main() {
+	c := make(chan struct{})
+
+	// Set up the HTML structure
+	setupHTML()
+
+	js.Global().Set("fetchUserData", js.FuncOf(fetchUserData))
+	js.Global().Set("toggleSideMenu", js.FuncOf(toggleSideMenu))
+	js.Global().Set("submitUserEdit", js.FuncOf(submitUserEdit))
+
+	// Add event listener for the buttons
 	js.Global().Get("document").Call("getElementById", "fetchDataBtn").Call("addEventListener", "click", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		fetchUserData(js.Value{}, []js.Value{})
 		return nil
@@ -34,10 +144,10 @@ func main() {
 		return nil
 	}))
 
-	//js.Global().Get("document").Call("getElementById", "createSideMenuBtn").Call("addEventListener", "click", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-	//	createSideMenu(js.Value{}, []js.Value{})
-	//	return nil
-	//}))
+	js.Global().Get("document").Call("getElementById", "submitEditBtn").Call("addEventListener", "click", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		submitUserEdit(js.Value{}, []js.Value{})
+		return nil
+	}))
 
 	<-c
 }
@@ -91,6 +201,20 @@ func setupHTML() {
 	output.Set("className", "output")
 	mainContent.Call("appendChild", output)
 
+	editForm := document.Call("createElement", "form")
+	editForm.Set("id", "editForm")
+	editForm.Get("style").Set("display", "none")
+	editForm.Set("innerHTML", `
+        <label for="userName">Name:</label>
+        <input type="text" id="userName" name="userName"><br>
+        <label for="userUsername">Username:</label>
+        <input type="text" id="userUsername" name="userUsername"><br>
+        <label for="userEmail">Email:</label>
+        <input type="email" id="userEmail" name="userEmail"><br>
+        <button type="button" id="submitEditBtn">Submit</button>
+    `)
+	mainContent.Call("appendChild", editForm)
+
 	// Replace the existing body with the new body
 	document.Get("documentElement").Call("replaceChild", newBody, document.Get("body"))
 }
@@ -110,55 +234,6 @@ func createDropdown(this js.Value, p []js.Value) interface{} {
 	return nil
 }
 
-func createSideMenu(this js.Value, p []js.Value) interface{} {
-	document := js.Global().Get("document")
-	sideMenu := document.Call("createElement", "div")
-	sideMenu.Set("id", "sideMenu")
-	sideMenu.Set("className", "sidemenu") // Ensure it uses the "sidemenu" class for styles
-	sideMenu.Get("style").Set("width", "200px")
-	sideMenu.Get("style").Set("height", "calc(100% - 60px)") // Height minus navbar height
-	sideMenu.Get("style").Set("top", "60px")                 // Position below the navbar
-	sideMenu.Get("style").Set("left", "0")
-	sideMenu.Get("style").Set("backgroundColor", "#111")
-	sideMenu.Get("style").Set("paddingTop", "20px")
-
-	menuItems := []string{"Home", "About", "Services", "Contact"}
-	for _, item := range menuItems {
-		menuItem := document.Call("createElement", "a")
-		menuItem.Set("textContent", item)
-		menuItem.Get("style").Set("padding", "8px 8px 8px 16px")
-		menuItem.Get("style").Set("textDecoration", "none")
-		menuItem.Get("style").Set("color", "white")
-		menuItem.Get("style").Set("display", "block")
-		menuItem.Get("style").Set("hover", "background-color: #575757;")
-
-		sideMenu.Call("appendChild", menuItem)
-	}
-
-	document.Get("body").Call("appendChild", sideMenu)
-	return nil
-}
-
-func hideSideMenu(this js.Value, p []js.Value) interface{} {
-	document := js.Global().Get("document")
-	sideMenu := document.Call("getElementById", "sideMenu")
-	if sideMenu.Truthy() {
-		sideMenu.Get("style").Set("display", "none")
-		document.Get("body").Get("classList").Call("remove", "shifted") // Remove the shifted class
-	}
-	return nil
-}
-
-func showSideMenu(this js.Value, p []js.Value) interface{} {
-	document := js.Global().Get("document")
-	sideMenu := document.Call("getElementById", "sideMenu")
-	if sideMenu.Truthy() {
-		sideMenu.Get("style").Set("display", "block")
-		document.Get("body").Get("classList").Call("add", "shifted") // Add the shifted class
-	}
-	return nil
-}
-
 func toggleSideMenu(this js.Value, p []js.Value) interface{} {
 	document := js.Global().Get("document")
 	sideMenu := document.Call("getElementById", "sideMenu")
@@ -173,51 +248,4 @@ func toggleSideMenu(this js.Value, p []js.Value) interface{} {
 	}
 	return nil
 }
-
-type User struct {
-	ID       int    `json:"id"`
-	Name     string `json:"name"`
-	Username string `json:"username"`
-	Email    string `json:"email"`
-}
-
-func fetchUserData(this js.Value, p []js.Value) interface{} {
-	go func() {
-		url := "http://localhost:8085/users/1"
-		resp, err := http.Get(url)
-		if err != nil {
-			onFetchUserDataError(err.Error())
-			return
-		}
-		defer resp.Body.Close()
-
-		if resp.StatusCode != http.StatusOK {
-			onFetchUserDataError("Non-OK HTTP status: " + resp.Status)
-			return
-		}
-
-		var user User
-		err = json.NewDecoder(resp.Body).Decode(&user)
-		if err != nil {
-			onFetchUserDataError("Failed to decode JSON: " + err.Error())
-			return
-		}
-
-		userJSON, err := json.Marshal(user)
-		if err != nil {
-			onFetchUserDataError("Failed to marshal user data: " + err.Error())
-			return
-		}
-
-		onFetchUserDataSuccess(string(userJSON))
-	}()
-	return nil
-}
-
-func onFetchUserDataSuccess(data string) {
-	js.Global().Get("document").Call("getElementById", "output").Set("innerText", data)
-}
-
-func onFetchUserDataError(errorMsg string) {
-	js.Global().Get("document").Call("getElementById", "output").Set("innerText", "Error: "+errorMsg)
-}
+*/
---
+++ client1/views/user/user.go
@@ -0,0 +1,119 @@
+package user
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"syscall/js"
+)
+
+type User struct {
+	ID       int    `json:"id"`
+	Name     string `json:"name"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
+
+type UserEditor struct {
+	CurrentUser User
+}
+
+func NewUserEditor() *UserEditor {
+	return &UserEditor{}
+}
+
+func (editor *UserEditor) FetchUserData(this js.Value, p []js.Value) interface{} {
+	go func() {
+		url := "http://localhost:8085/users/1"
+		resp, err := http.Get(url)
+		if err != nil {
+			editor.onFetchUserDataError(err.Error())
+			return
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			editor.onFetchUserDataError("Non-OK HTTP status: " + resp.Status)
+			return
+		}
+
+		var user User
+		err = json.NewDecoder(resp.Body).Decode(&user)
+		if err != nil {
+			editor.onFetchUserDataError("Failed to decode JSON: " + err.Error())
+			return
+		}
+
+		editor.CurrentUser = user
+
+		userJSON, err := json.Marshal(user)
+		if err != nil {
+			editor.onFetchUserDataError("Failed to marshal user data: " + err.Error())
+			return
+		}
+
+		editor.onFetchUserDataSuccess(string(userJSON))
+		editor.populateEditForm(user)
+	}()
+	return nil
+}
+
+func (editor *UserEditor) onFetchUserDataSuccess(data string) {
+	js.Global().Get("document").Call("getElementById", "output").Set("innerText", data)
+}
+
+func (editor *UserEditor) onFetchUserDataError(errorMsg string) {
+	js.Global().Get("document").Call("getElementById", "output").Set("innerText", "Error: "+errorMsg)
+}
+
+func (editor *UserEditor) populateEditForm(user User) {
+	document := js.Global().Get("document")
+	document.Call("getElementById", "userName").Set("value", user.Name)
+	document.Call("getElementById", "userUsername").Set("value", user.Username)
+	document.Call("getElementById", "userEmail").Set("value", user.Email)
+	document.Call("getElementById", "editForm").Get("style").Set("display", "block")
+}
+
+func (editor *UserEditor) SubmitUserEdit(this js.Value, p []js.Value) interface{} {
+	document := js.Global().Get("document")
+	name := document.Call("getElementById", "userName").Get("value").String()
+	username := document.Call("getElementById", "userUsername").Get("value").String()
+	email := document.Call("getElementById", "userEmail").Get("value").String()
+
+	editor.CurrentUser.Name = name
+	editor.CurrentUser.Username = username
+	editor.CurrentUser.Email = email
+
+	userJSON, err := json.Marshal(editor.CurrentUser)
+	if err != nil {
+		editor.onFetchUserDataError("Failed to marshal user data: " + err.Error())
+		return nil
+	}
+
+	go func() {
+		url := "http://localhost:8085/users/1"
+		req, err := http.NewRequest("PUT", url, bytes.NewBuffer(userJSON))
+		if err != nil {
+			editor.onFetchUserDataError("Failed to create request: " + err.Error())
+			return
+		}
+		req.Header.Set("Content-Type", "application/json")
+
+		client := &http.Client{}
+		resp, err := client.Do(req)
+		if err != nil {
+			editor.onFetchUserDataError("Failed to send request: " + err.Error())
+			return
+		}
+		defer resp.Body.Close()
+
+		if resp.StatusCode != http.StatusOK {
+			editor.onFetchUserDataError("Non-OK HTTP status: " + resp.Status)
+			return
+		}
+
+		editor.onFetchUserDataSuccess("User updated successfully")
+	}()
+
+	return nil
+}
