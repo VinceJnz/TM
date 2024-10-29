27fd178c8bd8a4651be786dcb903de4cd5c0f3fe
VinceJnz <vince.jennings@gmail.com>
2024-10-30 12:06:25 +1300

    fixed missing fields

--- api-server/models/admin.go
+++ api-server/models/admin.go
@@ -1,10 +1,16 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/guregu/null/v5/zero"
+)
 
 type Season struct {
 	ID       int       `json:"id" db:"id"`
 	Season   string    `json:"season" db:"season"`
+	StartDay zero.Int  `json:"start_day" db:"start_day"`
+	Length   zero.Int  `json:"length" db:"length"`
 	Created  time.Time `json:"created" db:"created"`
 	Modified time.Time `json:"modified" db:"modified"`
 }
