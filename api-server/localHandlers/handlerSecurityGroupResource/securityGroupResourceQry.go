e938359ba794b156b87c5f1dbba05748a3ea5fe6
VinceJnz <vince.jennings@gmail.com>
2024-11-27 15:22:18 +1300

    fixing security model var types

--- api-server/localHandlers/handlerSecurityGroupResource/securityGroupResourceQry.go
+++ api-server/localHandlers/handlerSecurityGroupResource/securityGroupResourceQry.go
@@ -6,7 +6,7 @@ import (
 
 func (h *Handler) GetAllQry() ([]models.GroupResource, error) {
 	records := []models.GroupResource{}
-	err := h.appConf.Db.Select(&records, `SELECT id, group_id, recource_id, access_level_id, access_type_id, admin_flag FROM st_group_resource`)
+	err := h.appConf.Db.Select(&records, `SELECT id, group_id, resource_id, access_level_id, access_type_id, admin_flag FROM st_group_resource`)
 	if err != nil {
 		return nil, err
 	}
@@ -15,7 +15,7 @@ func (h *Handler) GetAllQry() ([]models.GroupResource, error) {
 
 func (h *Handler) GetQry(id int) (models.GroupResource, error) {
 	record := models.GroupResource{}
-	err := h.appConf.Db.Get(&record, "SELECT id, group_id, recource_id, access_level_id, access_type_id, admin_flag FROM st_group_resource WHERE id = $1", id)
+	err := h.appConf.Db.Get(&record, "SELECT id, group_id, resource_id, access_level_id, access_type_id, admin_flag FROM st_group_resource WHERE id = $1", id)
 	if err != nil {
 		return models.GroupResource{}, err
 	}
@@ -24,13 +24,13 @@ func (h *Handler) GetQry(id int) (models.GroupResource, error) {
 
 func (h *Handler) CreateQry(record models.GroupResource) error {
 	err := h.appConf.Db.QueryRow(
-		"INSERT INTO st_group_resource (group_id, recource_id, access_level_id, access_type_id, admin_flag) VALUES ($1, $2, $3, $4, $5) RETURNING id",
+		"INSERT INTO st_group_resource (group_id, resource_id, access_level_id, access_type_id, admin_flag) VALUES ($1, $2, $3, $4, $5) RETURNING id",
 		record.GroupID, record.ResourceID, record.AccessLevelID, record.AccessTypeID, record.AdminFlag).Scan(&record.ID)
 	return err
 }
 
 func (h *Handler) UpdateQry(record models.GroupResource) error {
-	_, err := h.appConf.Db.Exec("UPDATE st_group_resource SET group_id = $1, recource_id = $2, access_level_id = $3, access_type_id = $4, admin_flag = $5 WHERE id = $6",
+	_, err := h.appConf.Db.Exec("UPDATE st_group_resource SET group_id = $1, resource_id = $2, access_level_id = $3, access_type_id = $4, admin_flag = $5 WHERE id = $6",
 		record.GroupID, record.ResourceID, record.AccessLevelID, record.AccessTypeID, record.AdminFlag, record.ID)
 	return err
 }
--- api-server/models/access.go
+++ api-server/models/access.go
@@ -1,31 +1,35 @@
 package models
 
-import "time"
+import (
+	"time"
+
+	"github.com/guregu/null/v5/zero"
+)
 
 type Group struct {
-	ID          int       `json:"id" db:"id"`
-	Name        string    `json:"name" db:"name"`
-	Description string    `json:"description" db:"description"`
-	AdminFlag   bool      `json:"admin_flag" db:"admin_flag"`
-	Created     time.Time `json:"created" db:"created"`
-	Modified    time.Time `json:"modified" db:"modified"`
+	ID          int         `json:"id" db:"id"`
+	Name        string      `json:"name" db:"name"`
+	Description zero.String `json:"description" db:"description"`
+	AdminFlag   bool        `json:"admin_flag" db:"admin_flag"`
+	Created     time.Time   `json:"created" db:"created"`
+	Modified    time.Time   `json:"modified" db:"modified"`
 }
 
 type UserGroup struct {
 	ID       int       `json:"id" db:"id"`
-	UserID   string    `json:"user_id" db:"user_id"`
-	GroupID  string    `json:"group_id" db:"group_id"`
+	UserID   int       `json:"user_id" db:"user_id"`
+	GroupID  int       `json:"group_id" db:"group_id"`
 	Created  time.Time `json:"created" db:"created"`
 	Modified time.Time `json:"modified" db:"modified"`
 }
 
 type GroupResource struct {
 	ID            int       `json:"id" db:"id"`
-	GroupID       string    `json:"group_id" db:"group_id"`
-	ResourceID    string    `json:"resource_id" db:"resource_id"`
-	AccessLevelID string    `json:"access_level_id" db:"access_level_id"`
-	AccessTypeID  string    `json:"access_type_id" db:"access_type_id"`
-	AdminFlag     string    `json:"admin_flag" db:"admin_flag"`
+	GroupID       int       `json:"group_id" db:"group_id"`
+	ResourceID    int       `json:"resource_id" db:"resource_id"`
+	AccessLevelID int       `json:"access_level_id" db:"access_level_id"`
+	AccessTypeID  int       `json:"access_type_id" db:"access_type_id"`
+	AdminFlag     bool      `json:"admin_flag" db:"admin_flag"`
 	Created       time.Time `json:"created" db:"created"`
 	Modified      time.Time `json:"modified" db:"modified"`
 }
