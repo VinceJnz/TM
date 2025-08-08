d1edb0c95a3c8e89a1ae1174a72cf336ddd9b8a2
VinceJnz <vince.jennings@gmail.com>
2025-08-08 15:01:28 +1200

    initial cut of webAuthn email reset client

---
+++ client2-NoSRP/views/account/webAuthnResetView/webAuthnResetView.go
@@ -0,0 +1,351 @@
+package webAuthnResetView
+
+import (
+	"client2-NoSRP/v2/app/appCore"
+	"client2-NoSRP/v2/app/eventProcessor"
+	"client2-NoSRP/v2/app/httpProcessor"
+	"client2-NoSRP/v2/views/utils/viewHelpers"
+	"log"
+	"syscall/js"
+)
+
+const debugTag = "webAuthnResetView."
+
+type ItemState int
+
+const (
+	ItemStateNone ItemState = iota
+	ItemStateFetching
+	ItemStateEditing
+	ItemStateAdding
+	ItemStateSaving
+	ItemStateDeleting
+	ItemStateSubmitted
+)
+
+type ViewState int
+
+const (
+	ViewStateNone ViewState = iota
+	ViewStateBlock
+)
+
+type ProcessStep int
+
+const (
+	ProcessStepGetUsername ProcessStep = iota
+	ProcessStepGetToken
+	ProcessStepFinished
+)
+
+type RecordState int
+
+const (
+	RecordStateReloadRequired RecordState = iota
+	RecordStateCurrent
+)
+
+// ********************* This needs to be changed for each api **********************
+const ApiURL = "/emailReset"
+
+// ********************* This needs to be changed for each api **********************
+type TableData struct {
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
+
+// ********************* This needs to be changed for each api **********************
+type UI struct {
+	Username js.Value
+	Email    js.Value
+}
+
+type Item struct {
+	Record TableData
+	//Add child structures as necessary
+}
+
+type ItemEditor struct {
+	appCore  *appCore.AppCore
+	client   *httpProcessor.Client
+	document js.Value
+
+	events        *eventProcessor.EventProcessor
+	CurrentRecord TableData
+	ItemState     ItemState
+	XXRecords     []TableData
+	XXItemList    []Item
+	UiComponents  UI
+	Div           js.Value
+	EditDiv       js.Value
+	//ListDiv       js.Value
+	StateDiv    js.Value
+	ParentID    int
+	ViewState   ViewState
+	RecordState RecordState
+}
+
+// NewItemEditor creates a new ItemEditor instance
+func New(document js.Value, eventProcessor *eventProcessor.EventProcessor, appCore *appCore.AppCore, idList ...int) *ItemEditor {
+	editor := new(ItemEditor)
+	editor.appCore = appCore
+	editor.document = document
+	editor.events = eventProcessor
+	editor.client = appCore.HttpClient //????????????????? to be removed ??????????????????
+
+	editor.ItemState = ItemStateNone
+
+	// Create a div for the item editor
+	editor.Div = editor.document.Call("createElement", "div")
+	editor.Div.Set("id", debugTag+"Div")
+
+	// Create a div for displaying the editor
+	editor.EditDiv = editor.document.Call("createElement", "div")
+	editor.EditDiv.Set("id", debugTag+"itemEditDiv")
+	editor.Div.Call("appendChild", editor.EditDiv)
+
+	// Create a div for displaying the list
+	//editor.ListDiv = editor.document.Call("createElement", "div")
+	//editor.ListDiv.Set("id", debugTag+"itemListDiv")
+	//editor.Div.Call("appendChild", editor.ListDiv)
+
+	// Create a div for displaying ItemState
+	editor.StateDiv = editor.document.Call("createElement", "div")
+	editor.StateDiv.Set("id", debugTag+"ItemStateDiv")
+	editor.Div.Call("appendChild", editor.StateDiv)
+
+	// Store supplied parent value
+	if len(idList) == 1 {
+		editor.ParentID = idList[0]
+	}
+
+	editor.RecordState = RecordStateReloadRequired
+
+	// Create child editors here
+	//..........
+
+	return editor
+}
+
+func (editor *ItemEditor) ResetView() {
+	editor.RecordState = RecordStateReloadRequired
+	editor.EditDiv.Set("innerHTML", "")
+	//editor.ListDiv.Set("innerHTML", "")
+}
+
+func (editor *ItemEditor) GetDiv() js.Value {
+	return editor.Div
+}
+
+func (editor *ItemEditor) Toggle() {
+	if editor.ViewState == ViewStateNone {
+		editor.ViewState = ViewStateBlock
+		editor.Display()
+	} else {
+		editor.ViewState = ViewStateNone
+		editor.Hide()
+	}
+}
+
+func (editor *ItemEditor) Hide() {
+	editor.Div.Get("style").Call("setProperty", "display", "none")
+	editor.ViewState = ViewStateNone
+}
+
+func (editor *ItemEditor) Display() {
+	editor.Div.Get("style").Call("setProperty", "display", "block")
+	editor.ViewState = ViewStateBlock
+}
+
+// NewItemData initializes a new item for adding
+func (editor *ItemEditor) NewItemData(this js.Value, p []js.Value) interface{} {
+	editor.updateStateDisplay(ItemStateAdding)
+	editor.CurrentRecord = TableData{}
+
+	// Set default values for the new record // ********************* This needs to be changed for each api **********************
+
+	editor.populateEditForm()
+	return nil
+}
+
+// ?????????????????????? document ref????????????
+/*
+func (editor *ItemEditor) NewDropdown(value int, labelText, htmlID string) (object, inputObj js.Value) {
+	// Create a div for displaying Dropdown
+	fieldset := editor.document.Call("createElement", "fieldset")
+	fieldset.Set("className", "input-group")
+
+	StateDropDown := editor.document.Call("createElement", "select")
+	StateDropDown.Set("id", htmlID)
+
+	for _, item := range editor.Records {
+		optionElement := editor.document.Call("createElement", "option")
+		optionElement.Set("value", item.ID)
+		optionElement.Set("text", item.Name)
+		if value == item.ID {
+			optionElement.Set("selected", true)
+		}
+		StateDropDown.Call("appendChild", optionElement)
+	}
+
+	// Create a label element
+	label := viewHelpers.Label(editor.document, labelText, htmlID)
+	fieldset.Call("appendChild", label)
+
+	fieldset.Call("appendChild", StateDropDown)
+
+	return fieldset, StateDropDown
+}
+*/
+
+// onCompletionMsg handles sending an event to display a message (e.g. error message or success message)
+func (editor *ItemEditor) onCompletionMsg(Msg string) {
+	editor.events.ProcessEvent(eventProcessor.Event{Type: "displayMessage", DebugTag: debugTag, Data: Msg})
+}
+
+// populateEditForm populates the item edit form with the current item's data
+func (editor *ItemEditor) populateEditForm() {
+	editor.EditDiv.Set("innerHTML", "") // Clear existing content
+	form := viewHelpers.Form(editor.SubmitItemEdit, editor.document, "editForm")
+
+	// Create input fields and add html validation as necessary // ********************* This needs to be changed for each api **********************
+	var localObjs UI
+
+	localObjs.Username, editor.UiComponents.Username = viewHelpers.StringEdit(editor.CurrentRecord.Username, editor.document, "Username", "text", "itemUsername")
+	editor.UiComponents.Username.Call("setAttribute", "required", "true")
+
+	localObjs.Email, editor.UiComponents.Email = viewHelpers.StringEdit(editor.CurrentRecord.Email, editor.document, "Email", "email", "itemEmail")
+	editor.UiComponents.Email.Call("setAttribute", "required", "true")
+
+	// Append fields to form // ********************* This needs to be changed for each api **********************
+	form.Call("appendChild", localObjs.Username)
+	form.Call("appendChild", localObjs.Email)
+
+	// Create submit button
+	submitBtn := viewHelpers.SubmitButton(editor.document, "Submit", "submitEditBtn")
+	cancelBtn := viewHelpers.Button(editor.cancelItemEdit, editor.document, "Cancel", "cancelEditBtn")
+
+	// Append elements to form
+	form.Call("appendChild", submitBtn)
+	form.Call("appendChild", cancelBtn)
+
+	// Append form to editor div
+	editor.EditDiv.Call("appendChild", form)
+
+	// Make sure the form is visible
+	editor.EditDiv.Get("style").Set("display", "block")
+}
+
+func (editor *ItemEditor) resetEditForm() {
+	// Clear existing content
+	editor.EditDiv.Set("innerHTML", "")
+
+	// Reset CurrentItem
+	editor.CurrentRecord = TableData{}
+
+	// Reset UI components
+	editor.UiComponents = UI{}
+
+	// Update state
+	editor.updateStateDisplay(ItemStateNone)
+}
+
+//func (editor *ItemEditor) ValidatePasswords(this js.Value, p []js.Value) interface{} {
+//	viewHelpers.ValidateNewPassword(editor.UiComponents.Password, editor.UiComponents.PasswordChk)
+//	return nil
+//}
+
+// SubmitItemEdit handles the submission of the item edit form
+func (editor *ItemEditor) SubmitItemEdit(this js.Value, p []js.Value) interface{} {
+	if len(p) > 0 {
+		event := p[0] // Extracts the js event object
+		event.Call("preventDefault")
+		log.Println(debugTag + "SubmitItemEdit()2 prevent event default")
+	}
+
+	// ********************* This needs to be changed for each api **********************
+	editor.CurrentRecord.Username = editor.UiComponents.Username.Get("value").String()
+	editor.CurrentRecord.Email = editor.UiComponents.Email.Get("value").String()
+
+	// Need to investigate the technique for passing values into a go routine ?????????
+	// I think I need to pass a copy of the current item to the go routine or use some other technique
+	// to avoid the data being overwritten etc.
+	//switch editor.ItemState {
+	//case ItemStateEditing:
+	//	go editor.UpdateItem(editor.CurrentRecord)
+	//case ItemStateAdding:
+	//	go editor.AddItem(editor.CurrentRecord)
+	//default:
+	//	editor.onCompletionMsg("Invalid item state for submission")
+	//}
+
+	// To request a reset link:
+	BeginWebAuthnEmailReset(editor.CurrentRecord.Email, func(success bool, message string) {
+		log.Println("Reset request:", success, message)
+		editor.onCompletionMsg(message)
+	})
+
+	editor.resetEditForm()
+	return nil
+}
+
+// cancelItemEdit handles the cancelling of the item edit form
+func (editor *ItemEditor) cancelItemEdit(this js.Value, p []js.Value) interface{} {
+	editor.resetEditForm()
+	return nil
+}
+
+// UpdateItem updates an existing item record in the item list
+func (editor *ItemEditor) UpdateItem(item TableData) {
+	/*
+		editor.updateStateDisplay(ItemStateSaving)
+		editor.client.NewRequest(http.MethodPut, ApiURL+"/"+strconv.Itoa(item.ID), nil, &item)
+		editor.RecordState = RecordStateReloadRequired
+		editor.FetchItems() // Refresh the item list
+		editor.updateStateDisplay(ItemStateNone)
+		editor.onCompletionMsg("Item record updated successfully")
+	*/
+}
+
+// AddItem adds a new item to the item list
+func (editor *ItemEditor) AddItem(item TableData) {
+	/*
+		editor.updateStateDisplay(ItemStateSaving)
+		editor.client.NewRequest(http.MethodPost, ApiURL, nil, &item)
+		editor.RecordState = RecordStateReloadRequired
+		editor.FetchItems() // Refresh the item list
+		editor.updateStateDisplay(ItemStateNone)
+		editor.onCompletionMsg("Item record added successfully")
+	*/
+}
+
+func (editor *ItemEditor) FetchItems() {
+	/*
+		if editor.RecordState == RecordStateReloadRequired {
+			editor.RecordState = RecordStateCurrent
+			// Fetch child data
+			//.....
+			go func() {
+				var records []TableData
+				editor.updateStateDisplay(ItemStateFetching)
+				editor.client.NewRequest(http.MethodGet, ApiURL, &records, nil)
+				editor.Records = records
+				editor.populateItemList()
+				editor.updateStateDisplay(ItemStateNone)
+			}()
+		}
+	*/
+}
+
+//func (editor *ItemEditor) deleteItem(itemID int) {
+//}
+
+//func (editor *ItemEditor) populateItemList() {
+//}
+
+func (editor *ItemEditor) updateStateDisplay(newState ItemState) {
+	editor.events.ProcessEvent(eventProcessor.Event{Type: "updateStatus", DebugTag: debugTag, Data: newState})
+	editor.ItemState = newState
+}
+
+// Event handlers and event data types
