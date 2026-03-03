fb9ba052a1e63ed15c29394973e09616ae40932f
VinceJnz <vince.jennings@gmail.com>
2026-03-03 13:24:36 +1300

    removed old code instances

--- client2-NoSRP/views/utils/viewHelpers/viewHelpersHtml.go
+++
@@ -1,7 +0,0 @@
-package viewHelpers
-
-import "syscall/js"
-
-func SetStyleProperty(element js.Value, property, setting string) {
-	element.Get("style").Call("setProperty", "display", "none")
-}
--- web-server/go.mod
+++
@@ -1,3 +0,0 @@
-module web-server/v2
-
-go 1.21.5
